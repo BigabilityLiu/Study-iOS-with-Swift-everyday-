21396aaaec249bfa9804c4d948ec7fc0fb0b681f
BigabilityLiu <1130471993@qq.com>
2015-10-24 14:19:24 +0800

    1024
    函数／对象／类

---
+++ 1024/1024Tests/_024Tests.swift
@@ -0,0 +1,36 @@
+//
+//  _024Tests.swift
+//  1024Tests
+//
+//  Created by 刘旭东 on 15/10/24.
+//  Copyright © 2015年 刘旭东. All rights reserved.
+//
+
+import XCTest
+@testable import _024
+
+class _024Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
